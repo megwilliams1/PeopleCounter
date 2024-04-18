7b8326b89f1df6554ecf04a49629f773f667a0e6
Megan Williams <mwilliams1.prsvr@gmail.com>
2024-04-17 19:59:57 -0500

    completed

---
+++ script.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+console.log(count);
+
+let myAge = 33;
+
+console.log(myAge);
+
+cmd+k+c
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+console.log(count)
+
+
+
+//let myAge = 33;
+//let humanDogRatio = 7;
+
+//let myDogAge = MyAge + humanDogRatio
+//console.log(myDogAge)
+
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoint = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints -75
+console.log(bonusPoints)
+bonusPoints = bonusPoints +45
+console.log(bonusPoints)
