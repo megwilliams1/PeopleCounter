2df4f0b8d6ed2bfe6771fd3f7b068c417a832f59
Megan Williams <mwilliams1.prsvr@gmail.com>
2024-04-22 20:02:32 -0500

    finished

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-let count = 0;
+// document.getElementById("count-el").innerText = 5
 
-console.log(count);
+// let countEl = document.getElementById("count-el")
+// let count = 0
+// function increment() {
+//      count = count + 1
+//      countEl.innerText = count
+// }
 
-let myAge = 33;
+// function save() {
+//     console.log(count)
+// }
 
-console.log(myAge);
+//let username = "per"
+//console.log(username)
 
-cmd+k+c
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch + secondBatch
-console.log(count)
+// let message = "You have three new notifications"
 
+// console.log(message)
 
 
-//let myAge = 33;
-//let humanDogRatio = 7;
+//let messageToUser = message + " , " + username + "!"
 
-//let myDogAge = MyAge + humanDogRatio
-//console.log(myDogAge)
+// let name = "Megan"
+// let greeting = "Hi, My name is "
+// let myGreeting = greeting + myName
 
-let bonusPoints = 50
-console.log(bonusPoints)
-bonusPoint = bonusPoints + 50
-console.log(bonusPoints)
-bonusPoints = bonusPoints -75
-console.log(bonusPoints)
-bonusPoints = bonusPoints +45
-console.log(bonusPoints)
+// console.log(myGreeting)
+
+// let points = 4
+// let bonusPoints = "10"
+
+// let totalPoints = points + bonusPoints
+
+// console.log(4 + 5) //9
+// console.log("2" + "4") // "24"
+// console.log("5" + 1) // "51"
+// console.log(100 + "100") // "100100"
+
+// 9
+// 24
+// 51
+// 100100
+
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Megan Williams"
+let greeting = "Welcome back"
+
+welcomeEl.innerText = greeting + myName
+
+
