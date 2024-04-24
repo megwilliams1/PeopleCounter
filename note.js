72fa248af6e9b95b855f8d980b483e07f23d6d45
Megan Williams <mwilliams1.prsvr@gmail.com>
2024-04-23 19:59:32 -0500

    completed

---
+++ note.js
@@ -0,0 +1,59 @@
+// document.getElementById("count-el").innerText = 5
+
+//let username = "per"
+//console.log(username)
+
+// let message = "You have three new notifications"
+
+// console.log(message)
+
+
+//let messageToUser = message + " , " + username + "!"
+
+// let name = "Megan"
+// let greeting = "Hi, My name is "
+// let myGreeting = greeting + myName
+
+// console.log(myGreeting)
+
+// let points = 4
+// let bonusPoints = "10"
+
+// let totalPoints = points + bonusPoints
+
+// console.log(4 + 5) //9
+// console.log("2" + "4") // "24"
+// console.log("5" + 1) // "51"
+// console.log(100 + "100") // "100100"
+
+// 9
+// 24
+// 51
+// 100100
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let name = "Megan Williams"
+// let greeting = "Welcome back"
+
+// welcomeEl.innerText = greeting + myName
+
+
+// welcomeEl.innerText = welcomeEl.innerText + "💕"
+
+
+let firstName = "Megan"
+let lastName = "Williams"
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+let name = "Linda"
+let gretting = "Hi there"
+
+function greetLinda() {
+    console.log(greeting + " , " + name + " ! " )
+}
+
+greetLinda()
+
--- script.js
+++ script.js
@@ -1,52 +1,15 @@
-// document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-// let countEl = document.getElementById("count-el")
-// let count = 0
-// function increment() {
-//      count = count + 1
-//      countEl.innerText = count
-// }
+function increment() {
+     count += 1
+     countEl.textContent = count
+}
 
-// function save() {
-//     console.log(count)
-// }
-
-//let username = "per"
-//console.log(username)
-
-// let message = "You have three new notifications"
-
-// console.log(message)
-
-
-//let messageToUser = message + " , " + username + "!"
-
-// let name = "Megan"
-// let greeting = "Hi, My name is "
-// let myGreeting = greeting + myName
-
-// console.log(myGreeting)
-
-// let points = 4
-// let bonusPoints = "10"
-
-// let totalPoints = points + bonusPoints
-
-// console.log(4 + 5) //9
-// console.log("2" + "4") // "24"
-// console.log("5" + 1) // "51"
-// console.log(100 + "100") // "100100"
-
-// 9
-// 24
-// 51
-// 100100
-
-let welcomeEl = document.getElementById("welcome-el")
-
-let name = "Megan Williams"
-let greeting = "Welcome back"
-
-welcomeEl.innerText = greeting + myName
-
-
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+ countEl.textContent = 0
+ count = 0
+}
